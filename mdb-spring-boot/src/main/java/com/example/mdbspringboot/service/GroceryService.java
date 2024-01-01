d41f78cdae6895b71e83c9715662a71b3cae275c
Anu Johar <joharanu@gmail.com>
2024-01-01 10:22:33 +0000

    added rest controller

---
+++ mdb-spring-boot/src/main/java/com/example/mdbspringboot/service/GroceryService.java
@@ -0,0 +1,54 @@
+package com.example.mdbspringboot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import com.example.mdbspringboot.model.GroceryItem;
+import com.example.mdbspringboot.repository.ItemRepository;
+import com.example.mdbspringboot.repository.CustomItemRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GroceryService {
+
+    @Autowired
+	ItemRepository groceryItemRepo;
+	
+	@Autowired
+	CustomItemRepository customRepo;
+
+    //CRUD  CREATE , READ , UPDATE , DELETE
+
+    @PostMapping("/items")
+    public GroceryItem addItem(GroceryItem item) {
+        item.setId(UUID.randomUUID().toString().split("-")[0]);
+        return groceryItemRepo.save(item);
+    }
+    @GetMapping("/items")
+    public List<GroceryItem> findAllItems() {
+        return groceryItemRepo.findAll();
+    }
+    @GetMapping("/items/{id}")
+    public GroceryItem getItembyId(String itemId){
+        return groceryItemRepo.findById(itemId).get();
+    }
+
+    public void updateItem(GroceryItem item){
+        //get the existing document from DB
+        // populate new value from request to existing object/entity/document
+        customRepo.updateItemQuantity(item.getName(),item.getItemQuantity());
+    }
+    @DeleteMapping("/employees/{id}")
+    public String deleteTask(String taskId){
+        groceryItemRepo.deleteById(taskId);
+        return taskId+" Grocery Item deleted from dashboard ";
+    }
+}
